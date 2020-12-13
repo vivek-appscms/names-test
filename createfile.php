4f81614cdd6d2f9a5735f2f3438ea6a89e5cf7c6
vivek-appscms <vivek@appscms.com>
2020-12-13 22:46:19 +0530

    fix by vivek

--- createfile.php
+++ createfile.php
@@ -1,6 +1,6 @@
 <?php
 
- for($i= 0; $i< 100000;$i++){
+ for($i= 0; $i< 50000;$i++){
     echo $i; echo "\n-------\n";
    $txt = "---\nlayout: test\npermalink: '$i'\n---";
    file_put_contents("pages/$i.md", $txt)or die("Unable to create markdown file!");
